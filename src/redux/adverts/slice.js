eb1669b8ef2eba2d7d89d65e7df5bed67dedf264
Movash <andrushhenko97@gmail.com>
2024-02-25 18:33:45 +0200

    add reducers and change state

--- src/redux/adverts/slice.js
+++ src/redux/adverts/slice.js
@@ -1,26 +1,35 @@
 import { createSlice } from '@reduxjs/toolkit';
-import { getAllAdverts, getFilter } from './thunks';
+import { getAllAdverts, getFilter, getTotalAdverts } from './thunks';
 
 const initialState = {
   adverts: [],
   total: null,
+  page: 1,
 };
 
 export const advertsSlice = createSlice({
   name: 'adverts',
   initialState,
+  reducers: {
+    setPage: (state, { payload }) => {
+      state.page = payload;
+    },
+    clearAdverts: state => {
+      state.adverts = [];
+    },
+  },
   extraReducers: builder => {
     builder.addCase(getAllAdverts.fulfilled, (state, { payload }) => {
       const newCars = payload.filter(
         car => !state.adverts.find(existingCar => existingCar.id === car.id)
       );
       state.adverts.push(...newCars);
-      const advertsLength = state.adverts.length
-      state.total = advertsLength;
     });
     builder.addCase(getFilter.fulfilled, (state, { payload }) => {
-      state.total = 1;
       state.adverts = payload;
+    });
+    builder.addCase(getTotalAdverts.fulfilled, (state, { payload }) => {
+      state.total = payload;
     });
   },
 });
