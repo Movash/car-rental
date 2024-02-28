ff88f5ca0eef68c4b3a2b82cec21cd93166137c3
Movash <andrushhenko97@gmail.com>
2024-02-28 17:42:44 +0200

    remove needless fetch

--- src/api/adverts.api.js
+++ src/api/adverts.api.js
@@ -4,17 +4,6 @@ axios.defaults.baseURL = 'https://65bbeca152189914b5bd569b.mockapi.io/api/v1/';
 
 const LIMIT = 12;
 
-export async function fetchAdverts(data) {
-  const page = data.page ? data.page : 1;
-  const res = await axios.get('adverts', {
-    params: {
-      page,
-      limit: LIMIT,
-    },
-  });
-  return res.data;
-}
-
 export async function fetchFilterAdverts(data) {
   const make = data.make;
   const page = data.page ? data.page : 1;
--- src/redux/adverts/thunks.js
+++ src/redux/adverts/thunks.js
@@ -1,17 +1,5 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import { fetchAdverts, fetchFilterAdverts, fetchTotalAdverts } from 'api/adverts.api';
-
-export const getAllAdverts = createAsyncThunk(
-  'adverts/fetchAll',
-  async (data, thunkAPI) => {
-    try {
-      const res = await fetchAdverts(data);
-      return res;
-    } catch (error) {
-      return thunkAPI.rejectWithValue(error.message);
-    }
-  }
-);
+import { fetchFilterAdverts, fetchTotalAdverts } from 'api/adverts.api';
 
 export const getFilter = createAsyncThunk(
   'adverts/fetchFilter',
